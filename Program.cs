ae0351536443739634dc9a02e36ad130335f71b5
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-09-17 14:22:18 +0200

    add proofs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,31 @@
-﻿void StampaArray(int[] array)
+﻿int[] numbers = { 2, 6, 7, 5, 3, 9 };
+
+// 1
+Console.WriteLine("Ecco la lista di numeri:");
+StampaArray(numbers);
+
+Console.WriteLine();
+
+// 2
+int[] squaredNumbers = ElevaArrayAlQuadrato(numbers);
+
+Console.WriteLine("Ecco la lista originale di numeri:");
+StampaArray(numbers);
+
+Console.WriteLine("Ecco la lista di numeri al quadrato:");
+StampaArray(squaredNumbers);
+
+Console.WriteLine();
+
+// 3
+int sumOfNumbers = SommaElementiArray(numbers);
+Console.WriteLine($"Ecco la somma di tutti i numeri: {sumOfNumbers}");
+
+// 4
+int sumOfSquaredNumbers = SommaElementiArray(squaredNumbers);
+Console.WriteLine($"Ecco la somma di tutti i numeri che sono stati elevati al quadrato: {sumOfSquaredNumbers}");
+    
+void StampaArray(int[] array)
 {
     Console.Write("[");
 
